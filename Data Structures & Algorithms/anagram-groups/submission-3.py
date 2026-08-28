e21063e16d0905fb837d45f01b33aa99e6a02989
sakuun_aah <91127128+amineaith3@users.noreply.github.com>
2026-08-28 16:34:16 +0100

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        def toAnag(s:str)->str:
+            return "".join(list(sorted([x for x in s])))
+        hmap = defaultdict(list)
+        for word in strs:
+            hmap[toAnag(word)].append(word)
+        ans = []
+        for _ , val in hmap.items():
+            ans.append(val)
+        return ans
