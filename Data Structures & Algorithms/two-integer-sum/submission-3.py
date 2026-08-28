1f30a2223035c21d16deb3c143dc7238e3f7f3f6
sakuun_aah <91127128+amineaith3@users.noreply.github.com>
2026-08-28 13:20:20 +0100

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        bruh = {}
+        for i, x in enumerate(nums):
+            if target - x in bruh:
+                return [bruh[target-x], i]
+            else:
+                bruh[x] = i
+        
