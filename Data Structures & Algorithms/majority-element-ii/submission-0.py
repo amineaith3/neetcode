9f5c256ff361b7288c58e828be8fd9d25840b69f
sakuun_aah <91127128+amineaith3@users.noreply.github.com>
2026-08-31 17:33:40 +0100

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        freq = defaultdict(int)
+        seen = set()
+        for num in nums:
+            freq[num] += 1
+            if freq[num] > len(nums) // 3:
+                if num not in seen:
+                    seen.add(num)
+        return list(seen)
