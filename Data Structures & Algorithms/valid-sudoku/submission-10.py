b73238b08ee7d5cccfa55f594e65ec18f872a1fd
sakuun_aah <91127128+amineaith3@users.noreply.github.com>
2026-08-31 16:51:43 +0100

    Add: valid-sudoku - submission-10

---
+++ Data Structures & Algorithms/valid-sudoku/submission-10.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        def isValidChunk(chunk:List[str])->bool:
+            seen = set()
+            for x in chunk:
+                if x in seen: return False
+                if x!='.': seen.add(x)
+            return True
+        cols = [[board[j][i] for j in range(9)] for i in range(9)]
+        squares = [[board[i+k][j+l] for k in range(3) for l in range(3) ]for i in range(0,9,3) for j in range(0,9,3)]
+        for row in board:
+            if not isValidChunk(row):return False
+        for col in cols:
+            if not isValidChunk(col):return False
+        for square in squares:
+            if not isValidChunk(square):return False
+        return True
