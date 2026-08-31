50bc7368d08b2a3d593ffe087e5325123d8bed3b
sakuun_aah <91127128+amineaith3@users.noreply.github.com>
2026-08-31 16:38:13 +0100

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        def isValidChunk(chunk:List[str])->bool:
+            for i in range(1, 10):
+                if chunk.count(str(i)) > 1:
+                    return False
+            return True
+        
+        #construct rows
+        rows = [row for row in board]
+
+        #construct cols
+        cols = []
+        for i in range(9):
+            col = []
+            for j in range(9):
+                col.append(board[j][i])
+            cols.append(col)
+        
+        #construct squares
+        squares = []
+        for i in range(0,9,3):
+            for j in range(0,9,3):
+                square = []
+                for k in range(3):
+                    for l in range(3):
+                        square.append(board[i+k][j+l])
+                squares.append(square)
+        
+        for row in rows:
+            if not isValidChunk(row):return False
+        for col in cols:
+            if not isValidChunk(col):return False
+        for square in squares:
+            if not isValidChunk(square):return False
+        return True
